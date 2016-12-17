077b012fd44f707ad6a1a58c63379a0cf6f0123f
Sriram <ssriram570@gmail.com>
2016-12-16 23:22:14 -0500

    split code into functions

--- bigram_vectorizer.py
+++ bigram_vectorizer.py
@@ -11,7 +11,11 @@ import numpy as np
 import sys
 
 
-def main():
+def construct_occur_matrix(file):
+	"""
+	builds a co-occurrence matrix b/w bigrams and sentences present in the input file
+	returns the built matrix
+	"""
 	# pre-defined Stop Words
 	stopwords = text.ENGLISH_STOP_WORDS.union("None", "blank", "\n")
 
@@ -20,8 +24,7 @@ def main():
 	sentences_length = []
 	summary = ""
 
-	text_file = open(sys.argv[1])
-
+	text_file = open(file)
 	# extracting corpus
 	for line in text_file:
 		text_corpus.append(line)
@@ -33,23 +36,45 @@ def main():
 	# transforming corpus to feature matrices
 	text_Matrix = bigram_vectorizer.fit_transform(text_corpus).toarray()
 
-	#print (bigram_vectorizer.get_feature_names())
-
 	# Our current matrix is a document-term matrix, we have to convert it into a term-document matrix
 														# for applying the SVD or SOFT-IMPUTE algorithms
 	texts = np.array(text_Matrix)
 	text_Transpose = texts.transpose()
 
 	## Finding the rank of the term-document matrix
-	print (np.linalg.matrix_rank(text_Transpose))
+	#print (np.linalg.matrix_rank(text_Transpose))
+
+	return text_Transpose
+
+
+def summarize():
+	"""
+	returns the summary of a text
+	"""
+	bigram_freq = []
+	sentences_length = []
+	summary = ""
+	text_corpus = []
+
+	text_file = sys.argv[1]
+
+	# extracting corpus
+	for line in open(text_file):
+		text_corpus.append(line)
+		sentences_length.append(len(line))	# sentence lengths computed
+
+	# constructing occurrence matrix
+	matrix = construct_occur_matrix(text_file)
 
 	# calculating bigram frequencies
-	n_rows, n_cols = text_Transpose.shape
+	n_rows, n_cols = matrix.shape
 	for i in range(n_rows):
-		bigram_freq.append(sum(text_Transpose[i]))
+		bigram_freq.append(sum(matrix[i]))
 
-	result = ilp_solve(text_Transpose, bigram_freq, sentences_length, 200)
+	# identifying the sentences to be included in the summary
+	result = ilp_solve(matrix, bigram_freq, sentences_length, 200)
 	
+	# Summary
 	print ("\nNumber of sentences selected for summarization: ", result['y'].count(1.0))
 
 	print ("\nSummarized Text:\n")
@@ -57,7 +82,7 @@ def main():
 		if val == 1:
 			summary += text_corpus[index]
 	
-	print (summary)
+	return summary
 
 if __name__ == '__main__':
-	main()
+	print(summarize())
