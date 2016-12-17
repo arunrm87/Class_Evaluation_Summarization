1c0f8ca2e41b81adb7b52cce35d6c754c42ed663
Arun <arun@arun-mbp.local>
2016-12-17 00:14:32 -0500

    Adding sparse-to-dense-matrix change

---
+++ sparse_to_dense.py
@@ -0,0 +1,90 @@
+"""
+Module to perform soft-impute of the sparse matrix
+"""
+from __future__ import print_function
+
+import numpy as np
+import copy
+from sklearn.utils.extmath import randomized_svd
+from bigram_vectorizer import *
+
+
+def check_convergence(old_matrix, new_matrix, threshold):
+	"""
+	old_matrix		:	A, the original matrix before reduction
+	new_matrix		:	B, matrix after reduction based on Frobenius norm
+	threshold		:	threshold value for finding convergence
+	Returns a boolean to indicate if there is convergence
+	"""
+	A = old_matrix
+	B = new_matrix
+	difference = A - B
+	change = np.sum(difference ** 2) ## Sum of squares of differences
+	new = np.sqrt(change)
+	old = np.sum(A ** 2) ## Sum of squares of original matrix
+	old = np.sqrt(old)
+	##old = np.sqrt((A ** 2).sum())
+	if (old != 0):
+		return ((new / old) < threshold)
+	else:
+		return True
+
+
+
+def svd(A, hyperparam):
+	"""
+	A			:	old matrix which has to be decomposed using SVD
+	hyperparam	:	Regularization parameter which prevents overfitting of data
+	Returns a new matrix of dimension k(obtained after SVD), and the value k
+	"""	
+	(U, s, V) = np.linalg.svd(A, full_matrices=False, compute_uv=True)
+	s_new = np.maximum(s - hyperparam, 0)
+	k = (s_new > 0).sum()
+	s_new = s_new[:k]
+	U_new = U[:, :k]
+	V_new = V[:k, :]
+	S_new = np.diag(s_new)
+	B = np.dot(U_new, np.dot(S_new, V_new))
+	return B, k
+	
+
+def dense(A, hyperparam, threshold, iterations):
+	"""
+	A			:	original term-document matrix (a sparse matrix)
+	hyperparam	:	Regularization parameter which prevents overfitting of data
+	threshold	:	threshold value for finding convergence	
+	iterations	:	Number of iterations before termination(if not converged)
+	Returns a dense matrix, with dimensions reduced from rank(r)
+	"""
+	for i in range(iterations):
+		B,rank = svd(A, hyperparam)
+		converged = check_convergence(A, B, threshold)
+		A = B
+		if (converged):
+			break
+	print (i)
+	return A
+
+
+def sparse_to_dense(summary):
+	text_copy = copy.deepcopy(summary)
+## Find a suitable value for the hyperparameter, some random value like 0.5, or based
+## on some heuristic like (rank of original matrix/10), or (max_singular_value of the
+##original matrix / 20)
+	_, s, _ = randomized_svd(summary, 1, n_iter=5)
+	hyperparameter = s[0] / 50
+
+	term_document_matrix_rank = np.linalg.matrix_rank(summary)
+	iterations = int(term_document_matrix_rank / 2) 
+
+
+	A_new = dense(text_copy, hyperparameter, 0.02, iterations)
+
+	##print (len(A_new) * len(A_new[0]))
+	##print (np.sum(A_new == 0))
+	##print (np.sum(A_new < 0))
+	##print (np.sum(summary == 0))
+	return A_new
+
+if __name__ == '__main__':
+	sparse_to_dense(construct_occur_matrix(sys.argv[1]))
