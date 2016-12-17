16f49d6325f9db241378b651c440af5dad4e9ea3
Sriram <ssriram570@gmail.com>
2016-12-16 19:39:58 -0500

    Deleted redundant files and modified bigram_vectorizer.py

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Class_Evaluation_Summarization
+
+IMPLEMENTATION OVERVIEW
+-----------------------
+    This implementation is based on the proposal made [here](http://aclweb.org/anthology/N/N16/N16-1010.pdf)
+    Since the proposed method in the paper is unsupervised, no training set is needed.
+
+DATASET OVERVIEW
+---------------- 
+    - Obtained from http://www.coursemirror.com/download/dataset
+    - The data includes both the students' responses and the standard summaries created by 
+    a teaching assistant.
+   
+REFERENCES
+----------
+Linear optimization function in Python - https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.optimize.linprog.html
--- bigram_vectorizer.py
+++ bigram_vectorizer.py
@@ -1,3 +1,6 @@
+"""
+Run using bigram_vectorizer.py <text_file>"
+"""
 from __future__ import print_function
 from sklearn import datasets
 from sklearn import metrics
@@ -7,79 +10,43 @@ import numpy as np
 import sys
 
 
+def main():
+	# pre-defined Stop Words
+	stopwords = text.ENGLISH_STOP_WORDS.union("None", "blank", "\n")
 
-## We don't follow the approach as in Assignment 3(using load_files), because we don't classify based on categories, so we just extract from the corresponding parsed document into a list and use it as a corpus
-#training_corpus = datasets.load_files("Training", description=None, load_content=True, encoding='utf-8', decode_error='ignore')
+	text_corpus = []	# training_corpus - information extracted from the parsed document
 
-## Preferred stopwords. The decision regarding removal of standard stopwords will be taken later
-stopwords = text.ENGLISH_STOP_WORDS.union("None", "blank", "\n")
+	bigram_freq = []
+	sentences_length = []
 
-## Training_corpus, which is just information extracted from the parsed document
-interest_corpus = []
-learning_corpus = []
-muddiest_corpus = []
+	text_file = open(sys.argv[1])
 
-bigram_freq = []
-sentences_length = []
-## Each summary has 3 documents, one with interesting points in the class, one with the muddiest points, and one with the points of learning.
-## We may later choose to combine all 3 documents into one
-interest_file = open("output1_interest")
+	# extracting corpus
+	for line in text_file:
+		text_corpus.append(line)
+		sentences_length.append(len(line))	# sentence lengths computed
 
-for line in interest_file:
-	interest_corpus.append(line)
-	sentences_length.append(len(line))
+	# Creating a document-term matrix based on bigram frequencies, with the use of custom stopwords
+	bigram_vectorizer = CountVectorizer(ngram_range=(2, 2), stop_words=stopwords)
 
-#print (interest_corpus)
-## Creating a document-term matrix based on bigram frequencies, with the use of custom stopwords
-bigram_vectorizer = CountVectorizer(ngram_range=(2, 2),stop_words=stopwords)
+	# transforming corpus to feature matrices
+	text_Matrix = bigram_vectorizer.fit_transform(text_corpus).toarray()
 
-interest_Matrix = bigram_vectorizer.fit_transform(interest_corpus).toarray()
+	#print (bigram_vectorizer.get_feature_names())
 
-## Our current matrix is a document-term matrix, we have to convert it into a term-document matrix for applying the SVD or SOFT-IMPUTE algorithms
-interest_Transpose = np.array(interest_Matrix)
-interest_Transpose = interest_Transpose.transpose()
+	# Our current matrix is a document-term matrix, we have to convert it into a term-document matrix
+														# for applying the SVD or SOFT-IMPUTE algorithms
+	texts = np.array(text_Matrix)
+	text_Transpose = texts.transpose()
 
-## Finding the rank of the term-document matrix
-print (np.linalg.matrix_rank(interest_Transpose))
+	## Finding the rank of the term-document matrix
+	print (np.linalg.matrix_rank(text_Transpose))
 
-# calculating bigram frequencies
-n_rows, n_cols = interest_Transpose.shape
-for i in range(n_rows):
-	bigram_freq.append(sum(interest_Transpose[i]))
-
-# ===================================================================================
-muddiest_file = open("output1_muddiest")
-
-for i in range(0,52):
-	muddiest_corpus.append(muddiest_file.readline())
-
-## Creating a document-term matrix based on bigram frequencies, with the use of custom stopwords
-bigram_vectorizer = CountVectorizer(ngram_range=(1, 1),stop_words=stopwords)
-
-muddiest_Matrix = bigram_vectorizer.fit_transform(muddiest_corpus).toarray()
-
-## Our current matrix is a document-term matrix, we have to convert it into a term-document matrix for applying the SVD or SOFT-IMPUTE algorithms
-muddiest_Transpose = np.array(muddiest_Matrix)
-muddiest_Transpose = muddiest_Transpose.transpose()
-
-## Finding the rank of the term-document matrix
-print (np.linalg.matrix_rank(muddiest_Transpose))
+	# calculating bigram frequencies
+	n_rows, n_cols = text_Transpose.shape
+	for i in range(n_rows):
+		bigram_freq.append(sum(text_Transpose[i]))
 
 
-
-learning_file = open("output1_learning")
-
-for i in range(0,52):
-	learning_corpus.append(learning_file.readline())
-
-## Creating a document-term matrix based on bigram frequencies, with the use of custom stopwords
-bigram_vectorizer = CountVectorizer(ngram_range=(1, 1),stop_words=stopwords)
-
-learning_Matrix = bigram_vectorizer.fit_transform(learning_corpus).toarray()
-
-## Our current matrix is a document-term matrix, we have to convert it into a term-document matrix for applying the SVD or SOFT-IMPUTE algorithms
-learning_Transpose = np.array(learning_Matrix)
-learning_Transpose = learning_Transpose.transpose()
-
-## Finding the rank of the term-document matrix
-print (np.linalg.matrix_rank(learning_Transpose))
+if __name__ == '__main__':
+	main()
